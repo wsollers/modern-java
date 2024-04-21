acab8e930b2ff89bbde1028be3b377635b2dd68c
BoondockTaints <wsollers@gmail.com>
2024-04-21 14:19:15 -0400

    Added ApplicationStart

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,13 +7,21 @@ plugins {
 }
 
 dependencies {
+
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-simple:2.0.13")
+    // https://mvnrepository.com/artifact/org.slf4j/slf4j-api
+    implementation("org.slf4j:slf4j-api:2.0.13")
+
     implementation("org.apache.commons:commons-text")
+
     implementation("javax.persistence:javax.persistence-api:2.2")
+
     implementation(project(":utilities"))
     implementation(project(":domain"))
 }
 
 application {
     // Define the main class for the application.
-    mainClass = "org.example.app.App"
+    mainClass = "dev.wsollers.application.ApplicationStart"
 }
