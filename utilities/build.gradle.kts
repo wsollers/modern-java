d2d14e20d4bc236f4077ec08e43e2c0759d6aa74
William E. Sollers III <wsollers@gmail.com>
2026-01-12 12:56:16 -0500

    See if these fix build on github

--- utilities/build.gradle.kts
+++ utilities/build.gradle.kts
@@ -7,8 +7,7 @@ repositories {
 }
 
 dependencies {
-    // Spring Boot 3.2.x manages commons-text version (and a lot more)
-    api(platform("org.springframework.boot:spring-boot-dependencies:3.2.12"))
+
 
 
     api("org.slf4j:slf4j-api")
